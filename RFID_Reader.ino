97633f18fac9ccb3bfbb1b4b65de686250eff5fe
KornWtp <elecwpr@gmail.com>
2019-12-27 21:37:52 +0700

    final

---
+++ RFID_Reader.ino
@@ -0,0 +1,57 @@
+/*
+  MEC10 Soil Moisture & Temperature & EC Sensor
+
+  version 1 - 3 Dec  2019
+  author : KornWtp
+
+*/
+
+#include <HardwareSerial.h>
+
+#define RX  16    //Serial Receive pin
+#define TX  17    //Serial Transmit pin
+
+HardwareSerial RS232Serial(Serial1);
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("RFID Reader");
+
+  RS232Serial.begin(38400, SERIAL_8E1, RX, TX);
+}
+
+void loop() {
+  // inquiry frame
+  RS232Serial.write(0xFA);
+  RS232Serial.write(0x07);
+  RS232Serial.write(0xFF);
+  RS232Serial.write(0x00);
+  RS232Serial.write(0x20);
+  RS232Serial.write(0x30);
+  RS232Serial.write(0x36);
+  RS232Serial.write(0xD7);
+
+  byte data_buf[30];
+  RS232Serial.readBytes(data_buf, 30);
+
+  for (byte i = 0; i < 30; i++ ) {
+    Serial.print(data_buf[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println("");
+  
+  // clear frame
+//  RS232Serial.write(0xFA);
+//  RS232Serial.write(0x07);
+//  RS232Serial.write(0xFF);
+//  RS232Serial.write(0x00);
+//  RS232Serial.write(0x20);
+//  RS232Serial.write(0x04);
+//  RS232Serial.write(0xE7);
+//  RS232Serial.write(0xA6);
+
+  
+  delay(5000);
+
+}
